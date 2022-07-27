d839a257056fbf4b50bbac97eafe9b4f028f32fc
DanieleKun <pucciodaniele95@gmail.com>
2022-07-27 10:33:44 +0200

    add edit view

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,9 +9,22 @@ class Post extends Model
 {
     use Slugger;
 
+    protected $fillable = [
+        'title', 'content', 'excerpt', 'category_id', 'image', 'slug'
+    ];
+
     // static protected $slugColumnName = 'percorso';
     public function category() {
         return $this->belongsTo('App\Models\Category');
+    }
+
+    public function users() {
+        return $this->belongsTo('App\Models\User');
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
     }
 
 }
